6643047b85b1bb3c3e13913c4b73d9b08a831d19
Rishit-5 <rishistar05@gmail.com>
2022-03-03 09:00:34 -0600

    expo image picker displaying image

--- Personalization.js
+++ Personalization.js
@@ -7,60 +7,91 @@ import * as ImagePicker from 'expo-image-picker';
 export default function Personalization(props) {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
-    const [image, setImage] = useState(null);
+    let [selectedImage, setSelectedImage] = React.useState(null);
+
     let openImagePickerAsync = async () => {
         let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
         if (permissionResult.granted === false) {
-            alert("Permission to access camera roll is required!");
+            alert('Permission to access camera roll is required!');
             return;
         }
 
         let pickerResult = await ImagePicker.launchImageLibraryAsync();
-        console.log(pickerResult);
-
-    }
-
-    const styles = StyleSheet.create({
-        input: {
-            height: 40,
-            width: 200,
-            margin: 6,
-            borderWidth: 1,
-            padding: 10,
-            color: "white",
-            borderColor: "white",
-
-        },
-        view: {
-            justifyContent: "center",
-            display: "flex",
-            alignItems: "center"
-        },
-        button: {
-            backgroundColor: 'blue',
-            padding: 20,
-            borderRadius: 5,
-        },
-        container:{
-            flex:1,
-            justifyContent: 'center',
-            alignItems: 'center',
+        if (pickerResult.cancelled === true) {
+            return;
         }
 
-    });
-    return (
-        <View style = {styles.container}>
-            <View>
+        setSelectedImage({ localUri: pickerResult.uri });
+    };
 
+    if (selectedImage !== null) {
+        return (
+            <View style={styles.container}>
                 <Text style = {{color: "white", textAlign: "center"}}>First Name</Text>
                 <TextInput style = {styles.input} value = {firstName} placeholder="First Name" onChangeText={text => setFirstName(text)}></TextInput>
                 <Text style = {{color: "white", textAlign: "center"}}>Last Name</Text>
-                <TextInput style = {styles.input} value = {lastName} placeholder="First Name" onChangeText={text => setLastName(text)}></TextInput>
+                <TextInput style = {styles.input} value = {lastName} placeholder="Last Name" onChangeText={text => setLastName(text)}></TextInput>
+                <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
                 <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
                     <Text style={styles.buttonText}>Pick a photo</Text>
                 </TouchableOpacity>
             </View>
+        );
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style = {{color: "white", textAlign: "center"}}>First Name</Text>
+            <TextInput style = {styles.input} value = {firstName} placeholder="First Name" onChangeText={text => setFirstName(text)}></TextInput>
+            <Text style = {{color: "white", textAlign: "center"}}>Last Name</Text>
+            <TextInput style = {styles.input} value = {lastName} placeholder="Last Name" onChangeText={text => setLastName(text)}></TextInput>
+            <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+            <TouchableOpacity onPress={openImagePickerAsync} style={styles.button}>
+                <Text style={styles.buttonText}>Pick a photo</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 305,
+        height: 159,
+        marginBottom: 20,
+    },
+    instructions: {
+        color: '#888',
+        fontSize: 18,
+        marginHorizontal: 15,
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: 'blue',
+        padding: 20,
+        borderRadius: 5,
+    },
+    buttonText: {
+        fontSize: 20,
+        color: '#fff',
+    },
+    thumbnail: {
+        width: 300,
+        height: 300,
+        resizeMode: 'contain',
+    },
+    input: {
+        height: 40,
+        width: 200,
+        margin: 6,
+        borderWidth: 1,
+        padding: 10,
+        color: "white",
+        borderColor: "white",
+    },
+});
